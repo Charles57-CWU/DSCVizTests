7a73c6fec4f23aceae5f1a13a69989833f8ace29
Charles57-CWU <84443326+Charles57-CWU@users.noreply.github.com>
2022-06-06 21:53:42 -0700

    Update Basic_Models.py

--- Basic_Models.py
+++ Basic_Models.py
@@ -1,5 +1,5 @@
 """
-This script will run a manually selected training-validation split against various classfication models from the scikit-learn library
+This script will run a manually selected training-validation split against various classification models from the scikit-learn library
 Add column headers, fix missing values, and make sure class labels are in numeric form, before running models
 Make sure there is only one class column, and the remaining columns are attributes you'd like to run in the model
 """
